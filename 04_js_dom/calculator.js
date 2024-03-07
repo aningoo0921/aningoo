8697b41d4c303b0a7ab922d07254f1ddc232b58d
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-03-07 22:39:30 +0900

    study:calculator

---
+++ 04_js_dom/calculator.js
@@ -0,0 +1,34 @@
+//js코드
+const result = document.querySelector("#result");
+const resultBtn = document.querySelector("#resultBtn");
+const eraseBtn = document.querySelector("#erase");
+
+resultBtn.addEventListener("click", function () {
+  const val1 = parseInt(document.getElementById("val1").value);
+  const val2 = parseInt(document.getElementById("val2").value);
+  const op = document.getElementById("op").value;
+  switch (op) {
+    case "+":
+      result.value = val1 + val2;
+      break;
+    case "-":
+      result.value = val1 - val2;
+      break;
+    case "*":
+      result.value = val1 * val2;
+      break;
+    case "/":
+      result.value = val1 / val2;
+      break;
+    case "%":
+      result.value = val1 % val2;
+      break;
+  }
+});
+
+eraseBtn.addEventListener("click", () => {
+  document.getElementById("val1").value = "";
+  document.getElementById("val2").value = "";
+  document.getElementById("op").value = "";
+  document.getElementById("result").value = "";
+});
