efc4b46824c62a83e320f113bafe75bdb2102e45
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-04-03 16:58:59 +0900

    study: create react

---
+++ 09_react_component/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ClassComponent from './components/ClassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassComponent />
+      <ClassComponent></ClassComponent>
+    </div>
+  );
+}
+
+export default ClassComponent;
