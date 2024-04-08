a31f1c02787f6e043bc90856451758aee4e289f5
Aningoo <aningooaningoo@Aningooui-noteubug.local>
2024-04-08 16:33:17 +0900

    study: state_characterPrac

---
+++ 11_react_state/src/App.js
@@ -0,0 +1,24 @@
+import ClassState from "./components/ClassState";
+import FuncState from "./components/FuncState";
+import Prac1 from "./components/Prac1";
+import CharactorsContainer from "./components/CharactorsContainer";
+import CharactorInfo from "./components/CharactorInfo";
+
+function App() {
+  return (
+    <div className="App">
+      < ClassState />
+      < FuncState />
+      < Prac1 />
+      < CharactorsContainer/>
+      < CharactorInfo />
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
---
+++ 11_react_state/src/components/CharactorInfo.jsx
@@ -0,0 +1,10 @@
+
+export default function CharactorInfo({ charactorInfo }) {
+  return (
+    <>
+      <div>Name: {charactorInfo.name}</div>
+      <div>Age: {charactorInfo.age}</div>
+      <div>NickName: {charactorInfo.nickName}</div>
+    </>
+  );
+}
